fb24f789a1922805af38a7f07b43dd91b94a97d7
JP Guiang <107418008+JP-Guiang@users.noreply.github.com>
2022-11-29 18:34:13 +1300

    Create individual post

---
+++ lara-blog/resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<title>Lara-blog</title>
+<link rel='stylesheet' href='app.css'>
+
+<body>
+  <h3><a href='/'>Home</a></h3>
+  <article>
+    <h1>This is your first route on Laravel yaaaaa</h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies a justo tristique maximus. Ut imperdiet tellus quis leo pretium, vitae dapibus nunc fermentum. Pellentesque viverra turpis non est viverra, imperdiet dictum velit egestas. Vestibulum vulputate urna eget dapibus efficitur. Proin est augue, viverra eget molestie et, viverra vitae odio. Vestibulum tincidunt rhoncus ante, at maximus dui suscipit sit amet. Mauris lorem sem, aliquam eu egestas ac, efficitur non erat.
+    
+    </p>
+  
+  </article>
+
+</body>
--- lara-blog/resources/views/welcome.blade.php
+++ lara-blog/resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <title>Lara-Blog</title>
 <link rel='stylesheet' href='app.css'>
-<script src='app.js'></script>
 
 <body>
   <h1>Pepe Test</h1>
