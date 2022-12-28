461f5cab1cec6bc09c57451e81a4a974308a58c0
JP Guiang <jp@icehouseventures.co.nz>
2022-12-28 14:22:29 +1300

    Dynamically load individual posts

--- lara-blog/resources/views/post.blade.php
+++ lara-blog/resources/views/post.blade.php
@@ -7,11 +7,7 @@
   <h1><a href="/">Home</a></h1>
 
   <article>
-    <h1><a href="/posts">My First Post</a></h1>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    </p>
+    <?= $post; ?>
   </article>
-
 
 </body>
