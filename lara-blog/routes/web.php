c90228789d39298dc749ce47639d0f7b906be470
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-12-28 23:56:05 +1300

    Add constraints for route path

--- lara-blog/routes/web.php
+++ lara-blog/routes/web.php
@@ -13,21 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('posts');
+  return view('posts');
 });
 
 Route::get('posts/{post}', function ($slug) {
-  
-  $path=__DIR__ . "/../resources/posts/{$slug}.html";
 
-  if(! file_exists($path)){
+  $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+  if (!file_exists($path)) {
     return redirect('/');
   }
 
-  $post=file_get_contents($path);
-  
-  return view('post',[
+  $post = file_get_contents($path);
+
+  return view('post', [
     'post' => $post
   ]);
-});
+})->where('post','[A-z_\-]+');
+
