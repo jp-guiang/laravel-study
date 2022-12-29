ff14f676adb5100c91426cb2632356ff9adde9f5
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-12-29 16:25:39 +1300

    Post using metadata

--- lara-blog/app/Models/Post.php
+++ lara-blog/app/Models/Post.php
@@ -4,28 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
 
+  public $title;
+
+  public $excerpt;
+
+  public $date;
+
+  public $body;
+
+  public $slug;
+
+  public function __construct($title, $excerpt, $date, $body, $slug)
+  {
+    $this->title = $title;
+    $this->excerpt = $excerpt;
+    $this->date = $date;
+    $this->body = $body;
+    $this->slug = $slug;
+  }
+
   public static function all()
   {
-    $files = File::files(resource_path('/posts'));
-
-    return array_map(fn ($file) => $file->getContents(), $files);
-
-    // return array_map(function ($file) {
-    //   return $file->getContents();
-    // }, $files);
+    $files = File::files(resource_path("posts/"));
+    return collect($files)
+      ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+      ->map(fn ($document) => new Post(
+        $document->title,
+        $document->excerpt,
+        $document->date,
+        $document->body(),
+        $document->slug
+      ));
   }
 
   public static function find($slug)
   {
 
-    if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-      throw new ModelNotFoundException();
-    }
+    $posts = static::all();
 
-    return cache()->remember("posts.{$slug}", now()->addHour(1), fn ()  => file_get_contents($path));
+    return $posts->firstWhere('slug', $slug);
   }
 }
--- lara-blog/routes/web.php
+++ lara-blog/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   $posts = Post::all();
+
   return view('posts', [
     'posts' => $posts
   ]);
