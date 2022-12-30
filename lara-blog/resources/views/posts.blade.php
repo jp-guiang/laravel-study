3a895d8218151e927fd009b72f7b21da7000580b
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-12-30 15:55:04 +1300

    Add layout blade component

--- lara-blog/resources/views/posts.blade.php
+++ lara-blog/resources/views/posts.blade.php
@@ -1,25 +1,17 @@
-<!doctype html>
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
-  <h1>This is the Posts Page</h1>
-  
+<x-layout>
   @foreach($posts as $post)
-    <article>
-      <h1>
-        <a href="posts/{{$post->slug}}">
-          {{$post->title}}
-        </a>
-        
-      </h1>
+  <article>
+    <h1>
+      <a href="posts/{{$post->slug}}">
+        {{$post->title}}
+      </a>
+      
+    </h1>
 
-      <div>
-        <?= $post->body; ?>
-      </div>
-    </article>
+    <div>
+      {!!$post->body!!}
+    </div>
+  </article>
   @endforeach
-  
-
-</body>
+</x-layout>
