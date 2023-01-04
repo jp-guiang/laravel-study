a2ab5a2c6fe62c53c23a06e2ce02c6f3bbb56e40
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2023-01-04 23:34:00 +1300

    Use getRouteKeyName()

--- lara-blog/app/Models/Post.php
+++ lara-blog/app/Models/Post.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $guarded = [];
+
+  public function getRouteKeyName()
+  {
+    return 'slug';
+  }
 }
--- lara-blog/routes/web.php
+++ lara-blog/routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
   ]);
 });
 
-Route::get('posts/{post:slug}', function (Post $post) {
+Route::get('posts/{post}', function (Post $post) {
 
   return view('post', [
 
