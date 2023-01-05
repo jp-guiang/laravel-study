7b54ff6f5dc4c0ff8d2d9c156b2080176841683a
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2023-01-06 00:26:03 +1300

    Change route to author username

--- lara-blog/resources/views/posts.blade.php
+++ lara-blog/resources/views/posts.blade.php
@@ -1,5 +1,7 @@
 
 <x-layout>
+  <h1><a href="/">Home</a></h1>
+
   @foreach($posts as $post)
   <article>
     <h1>
@@ -10,7 +12,7 @@
     </h1>
 
     <p>
-      By <a href="/authors/{{$post->author->id}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
+      By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
     </p>
 
     <div>
