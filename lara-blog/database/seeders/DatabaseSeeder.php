a034f54fc4b7acaaa6a356997be0f260c68dc8e1
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2023-01-05 23:34:01 +1300

    User factories instead of seed data

--- lara-blog/database/seeders/DatabaseSeeder.php
+++ lara-blog/database/seeders/DatabaseSeeder.php
@@ -17,52 +17,11 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
-
-    User::truncate();
-    Category::truncate();
-
-    $user = User::factory()->create();
-
-    $personal = Category::create([
-      'name' => 'Personal',
-      'slug' => 'personal',
+    $user = User::factory()->create([
+      'name' => 'Pepe'
     ]);
-
-    $famemely = Category::create([
-      'name' => 'Famemely',
-      'slug' => 'famemely',
-    ]);
-
-    $work = Category::create([
-      'name' => 'Work',
-      'slug' => 'work',
-    ]);
-
-    Post::create([
-      'user_id' => $user->id,
-      'category_id' => $famemely->id,
-      'title' => 'My Famemely Post',
-      'slug' => 'my-famemely-post',
-      'excerpt' => 'Famemely Excerpt',
-      'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
-    ]);
-
-    Post::create([
-      'user_id' => $user->id,
-      'category_id' => $personal->id,
-      'title' => 'My Personal Post',
-      'slug' => 'my-personal-post',
-      'excerpt' => 'Personal Excerpt',
-      'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
-    ]);
-
-    Post::create([
-      'user_id' => $user->id,
-      'category_id' => $work->id,
-      'title' => 'My Work Post',
-      'slug' => 'my-work-post',
-      'excerpt' => 'Work Excerpt',
-      'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>'
+    Post::factory(5)->create([
+      'user_id' => $user->id
     ]);
   }
 }
