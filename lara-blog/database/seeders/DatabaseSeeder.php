80f6e31ecf87647139d4b0ddbe3152ef42965914
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2023-01-10 13:19:39 +1300

    Integrating design

--- lara-blog/database/seeders/DatabaseSeeder.php
+++ lara-blog/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
     $user = User::factory()->create([
       'name' => 'Pepe'
     ]);
-    Post::factory(5)->create([
+    Post::factory(10)->create([
       'user_id' => $user->id
     ]);
   }
--- lara-blog/resources/views/posts.blade.php
+++ lara-blog/resources/views/posts.blade.php
@@ -1,23 +1,48 @@
-
 <x-layout>
-  <h1><a href="/">Home</a></h1>
 
-  @foreach($posts as $post)
-  <article>
-    <h1>
-      <a href="/posts/{{$post->slug}}">
-        {{$post->title}}
-      </a>
-      
-    </h1>
+  <!doctype html>
 
-    <p>
-      By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-    </p>
+<title>Laravel From Scratch Blog</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <div>
-      {!!$post->excerpt!!}
-    </div>
-  </article>
-  @endforeach
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        
+
+        @include('_posts-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+           
+          @if($posts->count())  
+          <x-post-featured-card :post="$posts[0]"/>
+
+          @if($posts->count()>1)
+            <div class="lg:grid lg:grid-cols-6">
+                @foreach($posts->skip(1) as $post)
+                  <x-post-card :post="$post" class="{{$loop->iteration<3 ?'col-span-3' : 'col-span-2'}}"/>
+                @endforeach
+            </div>
+          @endif
+
+          @else
+          <p class='text-center'>No posts yet please come back later</p>
+
+          @endif
+
+            {{-- <div class="lg:grid lg:grid-cols-3">
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
+                
+            </div> --}}
+        </main>
+
+        
+    </section>
+</body>
+
+
+  
 </x-layout>
