e5dcc975a3ad2de14bc3e161b88930be04147353
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2023-01-10 13:55:17 +1300

    Create posts-grid blade file

--- lara-blog/resources/views/posts.blade.php
+++ lara-blog/resources/views/posts.blade.php
@@ -1,48 +1,17 @@
-<x-layout>
+<x-layout>    
 
-  <!doctype html>
+  @include('_posts-header')
 
-<title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+      
+    @if($posts->count())  
+     <x-posts-grid :posts="$posts"/>
 
-<body style="font-family: Open Sans, sans-serif">
-    <section class="px-6 py-8">
-        
+    @else
+    <p class='text-center'>No posts yet please come back later</p>
 
-        @include('_posts-header')
+    @endif
 
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-           
-          @if($posts->count())  
-          <x-post-featured-card :post="$posts[0]"/>
+  </main>
 
-          @if($posts->count()>1)
-            <div class="lg:grid lg:grid-cols-6">
-                @foreach($posts->skip(1) as $post)
-                  <x-post-card :post="$post" class="{{$loop->iteration<3 ?'col-span-3' : 'col-span-2'}}"/>
-                @endforeach
-            </div>
-          @endif
-
-          @else
-          <p class='text-center'>No posts yet please come back later</p>
-
-          @endif
-
-            {{-- <div class="lg:grid lg:grid-cols-3">
-                <x-post-card/>
-                <x-post-card/>
-                <x-post-card/>
-                
-            </div> --}}
-        </main>
-
-        
-    </section>
-</body>
-
-
-  
 </x-layout>
